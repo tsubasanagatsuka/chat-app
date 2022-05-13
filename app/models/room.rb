627e8ba99e02c7e012f5cd40705c08336bce9ab3
tsubasanagatsuka <reach.283.0921@gmail.com>
2022-05-13 11:28:17 +0900

    room_usersテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,4 @@
 class Room < ApplicationRecord
+  has_many :room_users
+  has_many :rooms, througn: :room_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+
+  has_many :room_users
+  has_many :rooms, througn: :room_users
+  
 end
