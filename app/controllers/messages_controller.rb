d8b2ac64919647ea6caeb465bfef5131b22f2982
tsubasanagatsuka <reach.283.0921@gmail.com>
2022-05-15 02:37:22 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -19,7 +19,7 @@ class MessagesController < ApplicationController
  
    private
  
-   def message_params
-     params.require(:message).permit(:contentm, image).merge(user_id: current_user.id)
-   end
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
  end
